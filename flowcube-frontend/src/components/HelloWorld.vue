6b6d4f6f753704c483e2143c4007b10b5fb41494
jolofren <joakim@wiren.cc>
2022-12-16 14:53:47 +0100

    ehm ny init lol

---
+++ flowcube-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <v-app-bar elevation="5" rounded>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      </v-app-bar>
+      <v-title>test</v-title>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
