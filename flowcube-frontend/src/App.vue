72097d063da422826904dc7354e94c82bff3af3a
jolofren <joakim@wiren.cc>
2022-12-19 16:36:26 +0100

    InfoDump Component

--- flowcube-frontend/src/App.vue
+++ flowcube-frontend/src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <v-app>
     <v-main>
-      <select-cube @doThing="doThing"></select-cube>
+      <!-- <select-cube @doThing="doThing"></select-cube> -->
+      <div class="d-flex overflow-x-scroll">
+        <info-dump v-for="(infoItem, index) in info"
+          :key="index"
+          :info="infoItem"
+        >
+        </info-dump>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  import SelectCube from '@/components/SelectCube.vue'
+  // import SelectCube from '@/components/SelectCube.vue'
+  import InfoDump from '@/components/InfoDump.vue'
   export default ({
-    components: { SelectCube },
+    components: { 
+      // SelectCube,
+      InfoDump
+    },
     data() {
       return {
-
+        info: [
+          {
+            id: 0,
+            title: "Rubrik 1",
+            body: "lorem ipsum"
+          },
+          {
+            id: 1,
+            title: "Rubrik 2",
+            body: "dolor sit amet"
+          },
+          {
+            id: 1,
+            title: "Bajs",
+            body: "hej"
+          },
+        ]
       }
     },
     methods: {
---
+++ flowcube-frontend/src/components/InfoDump.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="h-screen min-width-screen d-flex align-center">
+    <div class="d-flex flex-column justify-center mx-auto text-max-width">
+      <h3 class="text-h3 text-center">{{ this.info.title }}</h3>
+      <p class="text-body-1"> {{ this.info.body }} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { list } from 'postcss';
+
+export default({
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    info: list,
+  }
+})
+</script>
+
+<style>
+.text-max-width {
+  max-width: 45ch;
+}
+
+/* fixa så att skiten inte är fucked */
+.min-width-screen {
+  min-width: 100vw;
+  scroll-snap-align: start;
+}
+
+.overflow-x-scroll {
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+}
+</style>
