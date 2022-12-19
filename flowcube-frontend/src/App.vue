f6cb525a22450e0512fe419ab167352ecaba293a
Joakim Wirén <joakim@wiren.cc>
2022-12-19 08:55:45 +0100

    POST request to trigger HomeAssistant

--- flowcube-frontend/src/App.vue
+++ flowcube-frontend/src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <select-cube @doThing="doThing"></select-cube>
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/SelectCube.vue'
+<script>
+  import SelectCube from '@/components/SelectCube.vue'
+  export default ({
+    components: { SelectCube },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      async doThing() { 
+        response = await fetch('http://192.168.1.54:9102/api/webhook/-TPgvqhjDcca7ZlKFd3WRnoZn', {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: ""
+        })
+        response();
+      }
+    }
+  })
 </script>
 
 <style>
--- flowcube-frontend/src/components/SelectCube.vue
+++ flowcube-frontend/src/components/SelectCube.vue
@@ -5,16 +5,16 @@
 
     </v-app-bar>
     <v-responsive class="d-flex align-center text-center fill-height">
-      <h3 class="text-h3 font-weight-bold mb-8">Choose an available Flow Cube</h3>
+      <h4 class="text-h4 font-weight-bold mb-8">Choose an available Flow Cube</h4>
       <v-sheet class="d-flex flex-column rounded-xl mx-auto pa-12" height="400" width="300" elevation="5">
         <h4 class="text-h4 font-weight-bold">Flow Cube 1</h4>
         <v-img src="./src/assets/logo.png"></v-img>
-        <v-btn class="font-weight-bold" variant="outlined">Choose Flow Cube</v-btn>
+        <v-btn @click="$emit('doThing')" class="font-weight-bold" variant="outlined">Choose Flow Cube</v-btn>
       </v-sheet>
     </v-responsive>
   </v-container>
 </template>
 
-<script setup>
-  //
+<script>
+
 </script>
