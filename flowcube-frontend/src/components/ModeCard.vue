1aed37060857c95035bb53adbba5c3e11e86f99f
Joakim Wirén <joakim@wiren.cc>
2023-01-12 09:03:42 +0100

    preview functionality on mode selection

--- flowcube-frontend/src/components/ModeCard.vue
+++ flowcube-frontend/src/components/ModeCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <v-sheet class="d-flex w-100 flex-column justify-space-between rounded-xl pa-8" elevation="5">
+    <v-sheet class="d-flex w-100 flex-column gap-8 rounded-xl pa-8" elevation="5">
       <h4 class="text-h5 font-weight-bold">{{ mode.name }}</h4>
       <p><v-icon icon="mdi-lightbulb-variant"></v-icon>{{ mode.lightingDescription }}</p>
       <p><v-icon icon="mdi-speaker"></v-icon>{{ mode.soundDescription }}</p>
-      <v-btn @click="$emit('selectMode')" color="white" class="font-weight-bold" variant="outlined">Select</v-btn>
+      <v-btn @click="$emit('previewMode')" color="white" class="font-weight-bold" variant="outlined">Preview</v-btn>
     </v-sheet>
   </div>
 </template>
@@ -16,3 +16,8 @@ export default {
   },
 }
 </script>
+<style scoped>
+.gap-8 {
+  gap: 0.5rem;
+}
+</style>
